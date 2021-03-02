a0d4caeca146d724af9e2e1d96d16cd239c39359
tanvirch0750 <tanvir0750@yahoo.com>
2021-03-03 02:42:31 +0600

    started stripe-submenu project and completed stripe context boilerplate, stripe navbar, stripe hero

---
+++ 13-stripe-submenus/src/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+import sublinks from "./data";
+
+const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="stripe" className="nav-logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {sublinks.map((sublink, idx) => {
+            return (
+              <li key={idx}>
+                <button className="link-btn">{sublink.page}</button>
+              </li>
+            );
+          })}
+        </ul>
+        <button className="btn signin-btn">Sign In</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
