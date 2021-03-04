f72a3878479d642e107eea27c24013080c347656
tanvirch0750 <tanvir0750@yahoo.com>
2021-03-05 00:47:57 +0600

    implemented clear cart functionality

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -15,10 +15,16 @@ const initialState = {
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // Action function
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
       }}
     >
       {children}
