5c3c83f85f90ed839d4e29d31044d87812a0b062
tanvirch0750 <tanvir0750@yahoo.com>
2021-03-10 12:32:10 +0600

    setup the markdown general setup

--- 16-markdown-preview/src/App.js
+++ 16-markdown-preview/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [markdown, setMarkdown] = useState("## Set Markdown Preview");
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">{markdown}</article>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
