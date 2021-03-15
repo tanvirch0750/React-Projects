2e538238f0fdc7effa03f8a39e19cdda265f8c30
tanvirch0750 <tanvir0750@yahoo.com>
2021-03-15 19:26:52 +0600

    implemented pagination paginate logic

--- 18-pagination/src/utils.js
+++ 18-pagination/src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemPerPage = 10;
+  const pages = Math.ceil(followers.length / itemPerPage);
 
-export default paginate
+  const newFollowers = Array.from({ length: pages }, (item, index) => {
+    const start = index * itemPerPage;
+    return followers.slice(start, start + itemPerPage);
+  });
+
+  return newFollowers;
+};
+
+export default paginate;
