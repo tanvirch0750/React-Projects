f551db8095119b0de287a5b7447f58cb6aedb193
tanvirch0750 <tanvir0750@yahoo.com>
2021-03-30 21:41:12 +0600

    implemented toggle functionality for dark theme

--- 20-dark-mode/src/App.js
+++ 20-dark-mode/src/App.js
@@ -3,12 +3,28 @@ import data from "./data";
 import Article from "./Article";
 
 function App() {
+  const [theme, setTheme] = useState("light-theme");
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>Dark World</h1>
-          <button className="btn">toggle theme</button>
+          <button className="btn" onClick={toggleTheme}>
+            toggle theme
+          </button>
         </div>
       </nav>
       <section className="articles">
